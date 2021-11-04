27896a88eae2daa4a696be61d3aaab0fa1e54876
Dylan Park <dylpark@protonmail.com>
2021-11-04 21:07:16 +1000

    Implementing typescript 'strict'

--- google/components/Avatar.tsx
+++ google/components/Avatar.tsx
@@ -1,7 +1,10 @@
 //
 //
+interface Props {
+  url: string;
+  }
 
-function Avatar({ url }) {
+const Avatar: React.FC<Props> = ({ url }) => {
   return <img 
   className="h-10 rounded-full cursor-pointer transition duration-150 ease-out transform hover:scale-110" 
   loading="lazy" 
