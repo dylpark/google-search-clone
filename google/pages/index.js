f8c794f17e5cde3903b5bd6eb4bcd743daada9fb
Dylan Park <dylpark@protonmail.com>
2021-11-04 20:10:58 +1000

    Creating the basic header
    Not a component yet

--- google/pages/index.js
+++ google/pages/index.js
@@ -10,9 +10,26 @@ export default function Home() {
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
 
-      <main className=""></main>
+      {/* <Header */}
+      <header className="flex w-full p-5 justify-between text-sm text-gray-700">
+        {/* Left Div */}
+        <div className="flex space-x-4 items-center">
+          <p className="link">About</p>
+          <p className="link">Store</p>
+        </div>
+        {/* Right Div */}
+        <div className="flex space-x-4 items-center">
+          <p className="link">Gmail</p>
+          <p className="link">Images</p>
+          {/* Icons */}
 
-      <footer className=""></footer>
+          {/* Avatar */}
+        </div>
+      </header>
+
+      {/* Body */}
+
+      {/* Footer */}
     </div>
   );
 }
