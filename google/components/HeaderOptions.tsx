542568281679b1b6cf6457c353dd03ae5753bbd6
Dylan Park <dylpark@protonmail.com>
2021-11-09 08:54:18 +1000

    Init HeaderOptions component

---
+++ google/components/HeaderOptions.tsx
@@ -0,0 +1,10 @@
+//
+//
+interface Props {
+}
+
+const HeaderOptions: React.FC<Props> = () => {
+    return <div></div>
+}
+
+export default HeaderOptions;
--- google/pages/search.tsx
+++ google/pages/search.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import SearchHeader from "../components/SearchHeader"
+import HeaderOptions from "../components/HeaderOptions"
 
 const Search: NextPage = () => {
 
@@ -13,6 +14,7 @@ const Search: NextPage = () => {
             </Head>
 
             <SearchHeader />
+            <HeaderOptions />
 
         </div>
     );
